2092b762ee06440fb15f6c2842a396b38af90307
Guillermina Fiore <guillefiore03@gmail.com>
2025-10-22 17:07:48 -0300

    Primer commit: HTML y JS

---
+++ script.js
@@ -0,0 +1,32 @@
+let tableContent = '';
+let totalProducts = [];
+const productsTableElem = document.querySelector('#productsTable'); /*Selector de CSS*/
+
+const goToProductDetails = (productId) => {
+    const selectedProduct = totalProducts.find(product => product.id === productId);
+    localStorage.setItem('selectedProduct', JSON.stringify(selectedProduct));
+    window.location.href = `product-details.html?id=${productId}`;
+}; 
+
+fetch('https://fakestoreapi.com/products')
+    .then(response => response.json())
+    .then(products => {
+        totalProducts = products;
+        products.forEach(product => {
+            tableContent += `<tr>
+            <td>
+                <a href="#" onclick="goToProductDetails(${product.id})">${product.title}></a>
+            </td>
+            <td>${product.price}</td>
+            <td>
+                <img
+                    src="${product.image}"
+                    alt="${product.title}"
+                    width="100"
+                    height="100">
+            </td>
+            </tr>`;
+        });
+        productsTableElem.innerHTML = tableContent;
+        console.log(tableContent);
+    });
